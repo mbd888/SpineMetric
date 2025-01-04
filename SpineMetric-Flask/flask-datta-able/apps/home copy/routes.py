4979e4f74780cf5835fc84b8cd789a422484eef2
mbd888 <130525830+mbd888@users.noreply.github.com>
2025-01-04 13:27:55 -0800

    Add files via upload

---
+++ SpineMetric-Flask/flask-datta-able/apps/home copy/routes.py
@@ -0,0 +1,85 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+import os, json, pprint
+
+from apps.home import blueprint
+from flask import render_template, request, redirect, url_for
+from flask_login import login_required
+from jinja2 import TemplateNotFound
+from flask_login import login_required, current_user
+from apps import db
+from apps.tasks import *
+
+@blueprint.route('/')
+@blueprint.route('/index')
+def index():
+    return render_template('pages/index.html', segment='index')
+
+@blueprint.route('/icon_feather')
+def icon_feather():
+    return render_template('pages/icon-feather.html', segment='icon_feather')
+
+@blueprint.route('/color')
+def color():
+    return render_template('pages/color.html', segment='color')
+
+@blueprint.route('/sample_page')
+def sample_page():
+    return render_template('pages/sample-page.html', segment='sample_page')
+
+@blueprint.route('/typography')
+def typography():
+    return render_template('pages/typography.html', segment='typography')
+
+@blueprint.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    if request.method == 'POST':
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        address = request.form.get('address')
+        bio = request.form.get('bio')
+
+        current_user.first_name = first_name
+        current_user.last_name = last_name
+        current_user.address = address
+        current_user.bio = bio
+
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+
+        return redirect(url_for('home_blueprint.profile'))
+
+    return render_template('pages/profile.html', segment='profile')
+
+
+# Helper - Extract current page name from request
+def get_segment(request):
+
+    try:
+
+        segment = request.path.split('/')[-1]
+
+        if segment == '':
+            segment = 'index'
+
+        return segment
+
+    except:
+        return None
+
+
+@blueprint.route('/tasks-test')
+def tasks_test():
+    
+    input_dict = { "data1": "04", "data2": "99" }
+    input_json = json.dumps(input_dict)
+
+    task = celery_test.delay( input_json )
+
+    return f"TASK_ID: {task.id}, output: { task.get() }"
